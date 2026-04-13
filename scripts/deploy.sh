985deec568d9bc5d56356df08e7fac0fa15bc8ab
ikennaenebuse <ikennastanley.enebuse@teledyne.com>
2026-04-13 11:07:46 +0100

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,24 +2,26 @@
 # Deployment script for updating the webapp after changes
 set -e
 
+
 WEB_APP_DIR="/var/www/sigmrbyte-webapp"
 
-# Pull latest changes
+# Go to project directory and pull latest changes
+cd "$WEB_APP_DIR"
 git pull
 
-cd "$WEB_APP_DIR"
-
 # Build frontend
+
 cd frontend
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-source ~/.bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install 20
 nvm use 20
 npm run build
 cd "$WEB_APP_DIR"
 
-# Reload Nginx (if config changed)
-sudo nginx -t && sudo systemctl reload nginx
+
+# Restart Nginx (if config changed)
+sudo nginx -t && sudo systemctl restart nginx
 
 # Renew SSL certificate (optional, safe to run anytime)
 sudo certbot renew --quiet
