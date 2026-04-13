f889134d8739d6edd2cbce60f231a4a5da27c1dd
ikennaenebuse <ikennastanley.enebuse@teledyne.com>
2026-04-13 11:10:14 +0100

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,8 +11,12 @@ git pull
 
 # Build frontend
 
+
 cd frontend
 export NVM_DIR="$HOME/.nvm"
+if [ ! -s "$NVM_DIR/nvm.sh" ]; then
+	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+fi
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm install 20
 nvm use 20
