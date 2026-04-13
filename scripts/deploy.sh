efe8af20a90ed58873e8cb8bfd487b3336ef88a9
ikennaenebuse <ikennastanley.enebuse@teledyne.com>
2026-04-13 10:50:02 +0100

    changed deploy script to use npm 20

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,10 @@ git pull
 
 # Build frontend
 cd frontend
-npm install
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+nvm install 20
+nvm use 20
 npm run build
 cd ..
 
